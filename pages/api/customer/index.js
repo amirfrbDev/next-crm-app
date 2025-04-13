3fb161e3fd69a395661c5c4180dd754c9e2036c0
Amir <amirmahdifarajbakhsh313@gmail.com>
2025-04-13 23:27:52 +0330

    new-user api developed

--- pages/api/customer/index.js
+++ pages/api/customer/index.js
@@ -1,11 +1,36 @@
+import Customer from "../../../models/Customer";
+import connectDB from "../../../utils/connectDB";
+
 export default async function handler(req, res) {
     try {
-
+        await connectDB()
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
+        console.logx(error.message);
+        return res.status(500).json({
             status: "failed",
             message: "Error in connecting to database"
         })
     }
+
+    if (req.method === "POST") {
+        const { data } = req.body;
+        if (!data.name || !data.lastName || !data.email) return res.status(400).json({ status: "failed", message: "Invalid Data!" })
+
+        try {
+            const customer = await Customer.create(data)
+            return res.status(201).json({
+                status: "success",
+                message: "Data created successfully",
+                data: customer
+            });
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({
+                status: "failed",
+                message: "Error in storing data!"
+            });
+        }
+    }
+
+
 }
