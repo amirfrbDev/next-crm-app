3fe02f17f6be52dec01fe0aec15320daccf55f79
Amir <amirmahdifarajbakhsh313@gmail.com>
2025-04-16 23:59:18 +0330

    API logic developed

--- pages/api/customer/[customerId].js
+++ pages/api/customer/[customerId].js
@@ -3,7 +3,7 @@ import Customer from "../../../models/Customer";
 export default async function handler(req, res) {
     if (req.method === "DELETE") {
         const { customerId } = req.query
-        console.log(req.query)
+
         if (!customerId) return res.status(400).json({
             status: "failed", message: "No Id"
         });
@@ -13,13 +13,16 @@ export default async function handler(req, res) {
         const { name, lastName, email, phone, address, zipCode, date, products } = req.body;
         const { customerId } = req.query;
 
+        const doesEmailExist = await Customer.findOne({ email });
+        if (doesEmailExist?._id && +doesEmailExist._id !== +customerId) return res.status(400).json({ status: "failed", message: "Email Already exist in database" })
 
-        const customer = await Customer.findOneAndUpdate(
+        const customer = await Customer.findByIdAndUpdate(
             customerId,
             { name, lastName, email, phone, address, zipCode, date, products },
             { new: true, runValidators: true }
         )
-        res.status(200).json({ message: "success", data:customer })
+
+        res.status(200).json({ message: "success", data: customer })
 
     }
 }
